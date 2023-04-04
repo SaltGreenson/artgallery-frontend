0b874d8ee4ea9de26c0d091b67a6d47a67f92af7
SaltGreenson <vladyuskovich@gmail.com>
2023-04-05 00:37:24 +0300

    storybook update

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -4,11 +4,6 @@ import { RouterContext } from "next/dist/shared/lib/router-context";
 
 const OriginalNextImage = NextImage.default;
 
-Object.defineProperty(NextImage, "default", {
-  configurable: true,
-  value: (props) => <OriginalNextImage {...props} unoptimized />,
-});
-
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
