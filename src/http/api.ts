e6e8c33853b74e0593aee4f575dc05379ff01996
SaltGreenson <vladyuskovich@gmail.com>
2023-04-23 12:35:20 +0300

    api url has been changed

--- src/http/api.ts
+++ src/http/api.ts
@@ -5,7 +5,7 @@ import { getFromStorage } from "@/utils/helpers/localStorage.helper";
 // https://artgallery-backend.onrender.com/api
 // http://localhost:5000/api
 export const API_URL =
-  process.env.API_URL || "https://dev-artgallery-backend.onrender.com";
+  process.env.API_URL || "https://dev-artgallery-backend.onrender.com/api";
 
 const api = axios.create({
   withCredentials: true,
